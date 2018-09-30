6098986c8b55ad1a2445c1396dddbe3782d77ce3
MieszkoSzyngwelski <mieszko.szyngwelski@interia.pl>
2018-09-30 14:17:03 +0200

    zadanie1

---
+++ zadanie1.py
@@ -0,0 +1,7 @@
+przysmak1=input("Podaj pierwszy ulubiony przysmak: ")
+
+przysmak2=input("Podaj drugi ulubiony przysmak: ")
+
+total = przysmak1 + przysmak2
+
+print ("twój nowy ulubiony przysmak to ", total)
